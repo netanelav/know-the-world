460d912702bc052d4ba6a5ce9ed00e12c73f81b9
netanel <netanel4747@gmail.com>
2019-08-28 16:27:18 +0300

    added the ability to see multiple results and more style

--- js/main.js
+++ js/main.js
@@ -4,60 +4,57 @@ $("#search-btn").click(function(e) {
 });
 
 function getCountry() {
-  $("#country-flag").html("");
-  $("#country-name").html("");
-  $("#country-data").html("");
+  $(".main").html("");
   let country = $("#query").val();
-  let settings = {
+
+  $.ajax({
+    type: "GET",
+    url: `https://restcountries-v1.p.rapidapi.com/name/${country}`,
     async: true,
     crossDomain: true,
-    url: `https://restcountries-v1.p.rapidapi.com/name/${country}`,
-    method: "GET",
     headers: {
       "x-rapidapi-host": "restcountries-v1.p.rapidapi.com",
       "x-rapidapi-key": "64bcf07ed2msh70ad2f22fea355ap132612jsn16c1cbecdd70"
+    },
+    success: function(response) {
+      console.log(response);
+      handleData(response);
+    },
+    error: function(response) {
+      alert("error");
     }
-  };
-
-  $.ajax(settings).done(function(response) {
-    console.log(response);
-    handleData(response);
   });
 }
 
 function handleData(response) {
-  let country = response[0];
-  let countryName = `${country.name} / ${country.nativeName}`;
-  let capital = country.capital;
-  let languages = country.languages;
-  // let borders = country.borders;
-  let region = country.region;
-  let callingCode = country.callingCodes;
-  let currencies = country.currencies;
-  let population = country.population;
-  let timezones = country.timezones;
-
-  let flagCode = country.alpha2Code;
-  let flag = `<img src="https://www.countryflags.io/${flagCode}/shiny/64.png">`;
-  $("#country-flag").append(flag);
-  $("#country-name").append(countryName);
-
-  let data = `<ul>
-    <li>Capital: ${capital}</li>
-    <li>Languages: ${languages}</li>
-    <li>Region: ${region}</li>
-    <li>Calling Code: ${callingCode}</li>
-    <li>Currency: ${currencies}</li>
-    <li>Population: ${population}</li>
-    <li>Timezone: ${timezones}</li>
-  </ul>`;
-  $("#country-data").append(data);
-  $(".box").css("visibility", "visible");
+  for (let i = 0; i < response.length; i++) {
+    let country = response[i];
+    let countryName = `${country.name} / ${country.nativeName}`;
+    let capital = country.capital;
+    let languages = country.languages;
+    let region = country.region;
+    let callingCode = country.callingCodes;
+    let currencies = country.currencies;
+    let population = country.population;
+    let timezones = country.timezones;
+    let flagCode = country.alpha2Code;
+    let box = `
+    <div class="col-4">
+      <div class="box">
+        <p id="country-flag"><img src="https://www.countryflags.io/${flagCode}/shiny/64.png"></p>
+        <p id="country-name">${countryName}</p>
+        <ul id="country-data">
+          <li>Capital: ${capital}</li>
+          <li>Languages: ${languages}</li>
+          <li>Region: ${region}</li>
+          <li>Calling Code: ${callingCode}</li>
+          <li>Currency: ${currencies}</li>
+          <li>Population: ${population}</li>
+          <li>Timezone: ${timezones}</li>
+        </ul>
+      </div>
+    </div>`;
+    $(".main").append(box);
+    $(".box").css("visibility", "visible");
+  }
 }
-
-{
-  /* <li>Borders: ${borders}</li> */
-}
-// function displayAllFlags() {
-
-// }
